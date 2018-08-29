069fe649da48f356d80f9a4a412220614d548732
Ilya Kulakov <kulakov.ilya@gmail.com>
2018-08-29 12:30:43 -0700

    Enable the test command.
    Tests can be run via:
    
    - python setup.py test
    - python -m unittest test
    - pytest
    - pytest test

--- test/__init__.py
+++ test/__init__.py
@@ -1,4 +1,13 @@
-from .test_case import *
-from .test_helpers import *
-from .test_mock import *
-from .test_selector import *
+import unittest
+
+test_modules = (
+    'test_case',
+    'test_helpers',
+    'test_mock',
+    'test_selector'
+)
+test_suite = unittest.defaultTestLoader.loadTestsFromNames(('{}.{}'.format(__name__, m) for m in test_modules))
+
+
+def load_tests(loader, tests, pattern):
+    return test_suite
