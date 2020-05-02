e6156551861ca6a6ac05ddfbd7d42d57ad21c74d
Martin Richard <martius@martiusweb.net>
2020-05-02 14:36:31 +0200

    Update helpers to support Python 3.8
    * remove deprecated uses of the loop arg,
    * remove usage of deprecated old-style coroutines.

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -7,23 +7,22 @@ import asynctest
 
 
 class TestExhaust(asynctest.TestCase):
-    @asyncio.coroutine
-    def wait_for(self, coro):
-        return (yield from asyncio.wait_for(coro, loop=self.loop, timeout=1))
+    async def wait_for(self, coro):
+        return await asyncio.wait_for(coro, timeout=1)
 
-    def test_exhaust_callbacks_nothing_to_wait(self):
+    async def test_exhaust_callbacks_nothing_to_wait(self):
         # Nothing ready, do nothing (must not timeout)
-        yield from self.wait_for(asynctest.helpers.exhaust_callbacks(self.loop))
+        await self.wait_for(asynctest.helpers.exhaust_callbacks(self.loop))
 
-    def test_exhaust_callbacks_one_callback(self):
+    async def test_exhaust_callbacks_one_callback(self):
         fut = asyncio.Future(loop=self.loop)
         self.loop.call_soon(fut.set_result, None)
 
         # A callback has been scheduled
-        yield from self.wait_for(asynctest.helpers.exhaust_callbacks(self.loop))
+        await self.wait_for(asynctest.helpers.exhaust_callbacks(self.loop))
         self.assertTrue(fut.done())
 
-    def test_exhaust_callbacks_cascading_callbacks(self):
+    async def test_exhaust_callbacks_cascading_callbacks(self):
         # A callback has been scheduled, then another (while the 1st is
         # running), we must wait for both
         fut = asyncio.Future(loop=self.loop)
@@ -31,7 +30,7 @@ class TestExhaust(asynctest.TestCase):
 
         fut.add_done_callback(lambda _: fut2.set_result(None))
         self.loop.call_soon(fut.set_result, None)
-        yield from self.wait_for(asynctest.helpers.exhaust_callbacks(self.loop))
+        await self.wait_for(asynctest.helpers.exhaust_callbacks(self.loop))
         self.assertTrue(fut.done())
         self.assertTrue(fut2.done())
 
